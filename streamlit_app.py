d3f3296a76d228f3a9e6701e9387163eaa80c881
Vicky Wu <62053512+wuyunfango@users.noreply.github.com>
2022-11-11 11:36:08 +1100

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberrt Oatmeal")
-streamlit.text("Kale, Spinich & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.text("🫐Omega 3 & Blueberrt Oatmeal")
+streamlit.text("🥙Kale, Spinich & Rocket Smoothie")
+streamlit.text("🥚Hard-Boiled Free-Range Egg")
+streamlit.text("🥑Avocado Toast")
