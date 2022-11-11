21951c07f501147ccba814d4b3d3c677a3c76092
Vicky Wu <62053512+wuyunfango@users.noreply.github.com>
2022-11-11 11:24:15 +1100

    Update streamlit_app.py
    corrected error

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 & Blueberrt Oatmeal")
-streamlit.text(""Kale, Spinich & Rocket Smoothie")
+streamlit.text("Kale, Spinich & Rocket Smoothie")
 streamlit.text("Hard-Boiled Free-Range Egg")
