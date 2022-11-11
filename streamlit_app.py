14613deddc573079f44129d0e5e146cd558965db
Vicky Wu <62053512+wuyunfango@users.noreply.github.com>
2022-11-11 11:45:49 +1100

    Update streamlit_app.py
    created fruit picking iteractions

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,9 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# makeing a pick list here so customers can pick the fruits for smoothie
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list))
+
+# display the table on the page
 streamlit.dataframe(my_fruit_list)
