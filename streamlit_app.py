b3b6b5a3396c290255432eec88f03c65017ec2e8
Vicky Wu <62053512+wuyunfango@users.noreply.github.com>
2022-11-11 11:36:39 +1100

    Update streamlit_app.py
    added spaces

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
-streamlit.text("🫐Omega 3 & Blueberrt Oatmeal")
-streamlit.text("🥙Kale, Spinich & Rocket Smoothie")
-streamlit.text("🥚Hard-Boiled Free-Range Egg")
-streamlit.text("🥑Avocado Toast")
+streamlit.text("🫐 Omega 3 & Blueberrt Oatmeal")
+streamlit.text("🥙 Kale, Spinich & Rocket Smoothie")
+streamlit.text("🥚 Hard-Boiled Free-Range Egg")
+streamlit.text("🥑 Avocado Toast")
